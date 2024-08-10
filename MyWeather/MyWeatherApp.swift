ee13fb1e5cabbacfbf636359d6d4664252568da5
pratama6624 <scyoung6624@gmail.com>
2024-08-10 21:33:26 +0700

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Pratama One on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
